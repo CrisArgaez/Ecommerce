854a851543f8ba341fbcfddd07be4992cbeadbfd
Cristofher <al068797@uacam.mx>
2024-05-23 00:56:14 -0600

    endpoints del carrito agregados y modificaciones al dao carrito hechos

---
+++ Backend/API_Ecommerce/src/main/java/com/example/programacionweb_its_prac1/carritoServlet.java
@@ -0,0 +1,114 @@
+package com.example.programacionweb_its_prac1;
+
+import dao.CarritoDAO;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@WebServlet("/carrito/*")
+public class carritoServlet extends HttpServlet {
+
+    private final JsonResponse jResp = new JsonResponse();
+    private final CarritoDAO carritoDAO = new CarritoDAO();
+
+    private void addCorsHeaders(HttpServletResponse response) {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        response.setHeader("Access-Control-Max-Age", "3600"); // Tiempo de caché para preflight requests
+    }
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        addCorsHeaders(resp);
+        resp.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        addCorsHeaders(resp);
+        resp.setContentType("application/json");
+
+        if (req.getPathInfo().equals("/")) {
+            obtenerArticulosCarrito(req, resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        addCorsHeaders(resp);
+        resp.setContentType("application/json");
+        // Procesar la compra de los artículos en el carrito
+        realizarCompra(req, resp);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        addCorsHeaders(resp);
+        resp.setContentType("application/json");
+
+        // Actualizar la cantidad de elementos del artículo en el carrito
+        String pathInfo = req.getPathInfo();
+        if (pathInfo != null && pathInfo.length() > 1) {
+            actualizarCantidadArticulo(req, resp);
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        addCorsHeaders(resp);
+        resp.setContentType("application/json");
+
+        // Eliminar un artículo del carrito
+        String pathInfo = req.getPathInfo();
+        if (pathInfo != null && pathInfo.length() > 1) {
+            eliminarArticuloCarrito(req, resp);
+        }
+    }
+
+    private void obtenerArticulosCarrito(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String[] idProductosStr = req.getParameterValues("idProductos");
+        List<Integer> idProductos = Arrays.stream(idProductosStr).map(Integer::parseInt).collect(Collectors.toList());
+        List<Productos> productos = carritoDAO.consultarPorIds(idProductos);
+        jResp.success(req, resp, "Listado de productos en el carrito: ", productos);
+    }
+
+    private void realizarCompra(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String pathInfo = req.getPathInfo();
+        int cantidad = Integer.parseInt(req.getParameter("existencia"));
+        if(cantidad > 0) {
+            jResp.success(req, resp, "Compra realizada con éxito.", "");
+            actualizarCantidadArticulo(req, resp);
+        }
+    }
+
+    private void actualizarCantidadArticulo(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String pathInfo = req.getPathInfo();
+        int idProducto = Integer.parseInt(pathInfo.substring(1));
+        int nuevaCantidad = Integer.parseInt(req.getParameter("existencia"));
+
+        int filasActualizadas = carritoDAO.actualizarCantidad(idProducto, nuevaCantidad);
+        if (filasActualizadas > 0) {
+            jResp.success(req, resp, "Cantidad actualizada con éxito.", nuevaCantidad);
+        }
+    }
+
+
+
+    private void eliminarArticuloCarrito(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String pathInfo = req.getPathInfo();
+        int idProducto = Integer.parseInt(pathInfo.substring(1));
+
+        int filasEliminadas = carritoDAO.eliminarProductoCarrito(idProducto);
+        if (filasEliminadas > 0) {
+            jResp.success(req, resp, "Producto eliminado con éxito.", "");
+
+        }
+    }
+}
+
--- Frontend/tienda.js
+++ Frontend/tienda.js
@@ -4,44 +4,7 @@ const productos = [
       titulo: "Producto 1",
       imagen: "https://i.pinimg.com/564x/31/d6/3a/31d63a2d2606abf32cf7ab6a614e3075.jpg",
       precio: 29.99,
-    },
-
-    {
-        titulo: "Producto 2",
-        imagen: "https://m.media-amazon.com/images/I/71Lg7pdC+6L._AC_SX679_.jpg",
-        precio: 29.99,
-      },
-      {
-        titulo: "Producto 3",
-        imagen: "https://i.pinimg.com/564x/31/d6/3a/31d63a2d2606abf32cf7ab6a614e3075.jpg",
-        precio: 29.99,
-      },
-      {
-        titulo: "Producto 4",
-        imagen: "https://m.media-amazon.com/images/I/51yvzJj5EIL._AC_SX679_.jpg",
-        precio: 29.99,
-      },
-      {
-        titulo: "Producto 5",
-        imagen: "https://m.media-amazon.com/images/I/61FUEXIELzL.__AC_SY445_SX342_QL70_ML2_.jpg",
-        precio: 29.99,
-      },
-      {
-        titulo: "Producto 6",
-        imagen: "https://m.media-amazon.com/images/I/61V5FRUgX8L._AC_SX679_.jpg",
-        precio: 29.99,
-      },
-      {
-        titulo: "Producto 7",
-        imagen: "https://m.media-amazon.com/images/I/91im+xo9jQL._AC_SX569_.jpg",
-        precio: 29.99,
-      },
-      {
-        titulo: "Producto 8",
-        imagen: "https://m.media-amazon.com/images/I/61FUEXIELzL.__AC_SY445_SX342_QL70_ML2_.jpg",
-        precio: 29.99,
-      },
-    // ... otros productos
+    }
   ];
   
   // Funcion que crea las tarjetas para los productos y mostralas
