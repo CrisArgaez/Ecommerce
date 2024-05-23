f03b444a64a3fc10e95af4ab55ba8419e232b05f
cleomari <@al062090.uacam.mx>
2024-05-23 17:38:12 -0600

    conflicto resuelto

--- Frontend/tienda.js
+++ Frontend/tienda.js
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-// Supongamos que tienes un array de objetos con los datos de tus productos
-const productos = [
-    {
-      titulo: "Producto 1",
-      imagen: "https://i.pinimg.com/564x/31/d6/3a/31d63a2d2606abf32cf7ab6a614e3075.jpg",
-      precio: 29.99,
-    }
-  ];
-  
-  // Funcion que crea las tarjetas para los productos y mostralas
-  function crearProductos() {
-    const containerItems = document.querySelector(".container-items"); //Contenedor de los productos
-  
-    productos.forEach((producto) => {
-      const item = document.createElement("div"); // Crea el contenedor de cada producto
-      item.classList.add("product-box"); // Agrega la clase "producto"
-=======
 document.addEventListener('DOMContentLoaded', async function() {
     try {
         const url = "http://localhost:8080/api/articulos";
@@ -26,7 +8,6 @@ document.addEventListener('DOMContentLoaded', async function() {
                 "Content-Type": "application/json"
             },
         });
->>>>>>> 57c151d9ff475fa8c8b121a249fdc0fcb3b5c473
 
         const responseData = await response.json();
         console.log(responseData.message)//Imprimir el valor del json "message"
