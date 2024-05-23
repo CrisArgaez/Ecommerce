e97dd3237e5fa3fac87548cb246cec0f9c892080
CrisArgaez <150169887+CrisArgaez@users.noreply.github.com>
2024-05-22 22:39:58 -0600

    Recuperacion de los articulos de la BD

--- Backend/API_Ecommerce/src/main/java/dao/CarritoDAO.java
+++ Backend/API_Ecommerce/src/main/java/dao/CarritoDAO.java
@@ -16,7 +16,7 @@ public class CarritoDAO{
         conexion = new Conexion<Productos>();
     }
 
-    public Productos consultar(int idProducto) {
+    /*public Productos consultar(int idProducto) {
         String query = "SELECT id_producto, nombre, descripcion, precio, existencia FROM carrito_de_compras WHERE id_producto = ?";
         ArrayList<ArrayList<String>> registros = conexion.ejecutarConsulta(query, new String[]{String.valueOf(idProducto)});
 
@@ -28,7 +28,7 @@ public class CarritoDAO{
             int precio = Integer.parseInt(registro.get(3));
             int existencia = Integer.parseInt(registro.get(4));
 
-            return new Productos(id, nombre, "", "", descripcion, precio, existencia);
+            return new Productos(id, nombre, "", "", descripcion, precio, existencia, galeriaFotos);
         }
 
         return null;
@@ -42,6 +42,6 @@ public class CarritoDAO{
     public int actualizarCantidad(int idProducto, int nuevaCantidad) {
         String query = "UPDATE carrito_de_compras SET existencia = ? WHERE id_producto = ?";
         return conexion.ejecutarActualizacion(query, new String[]{String.valueOf(nuevaCantidad), String.valueOf(idProducto)});
-    }
+    }*/
 }
 
--- Backend/API_Ecommerce/src/main/java/dao/ProductosDAO.java
+++ Backend/API_Ecommerce/src/main/java/dao/ProductosDAO.java
@@ -1,21 +1,25 @@
 package dao;
 
 import java.util.ArrayList;
-import java.util.List;
 import com.example.programacionweb_its_prac1.Productos;
 import conexion.Conexion;
 
-public class ProductosDAO {
+public class ProductosDAO implements DAOGeneral<Integer, Productos, String> {
     private final Conexion c;
 
     public ProductosDAO() {
         c = new Conexion<Productos>();
     }
 
-    public List<Productos> mostrarInformacion() {
-        String query = "SELECT id_producto, nombre, url_imagen, especificacion, descripcion, precio, existencia FROM productos";
+    @Override
+    public int agregar(Productos elemento) {
+        return 0;
+    }
+
+    public ArrayList<Productos> consultar() {
+        String query = "SELECT * FROM productos";
         ArrayList<ArrayList<String>> registros = c.ejecutarConsulta(query, null);
-        List<Productos> productos = new ArrayList<>();
+        ArrayList<Productos> productos = new ArrayList<>();
 
         for (ArrayList<String> registro : registros) {
             int id = Integer.parseInt(registro.get(0));
@@ -25,11 +29,33 @@ public class ProductosDAO {
             String descripcion = registro.get(4);
             int precio = Integer.parseInt(registro.get(5));
             int existencia = Integer.parseInt(registro.get(6));
+            String galeriaFotos = registro.get(7);
 
-            Productos producto = new Productos(id, nombre, urlImagen, especificacion, descripcion, precio, existencia);
+            Productos producto = new Productos(id, nombre, urlImagen, especificacion, descripcion, precio, existencia, galeriaFotos);
             productos.add(producto);
         }
         return productos;
+    }
+
+
+    @Override
+    public Productos consultar(Integer id) {
+        return null;
+    }
+
+    @Override
+    public Productos consultarCorreo(String correo) {
+        return null;
+    }
+
+    @Override
+    public int actualizar(Integer id, Productos elemento) {
+        return 0;
+    }
+
+    @Override
+    public int eliminar(Integer id) {
+        return 0;
     }
 }
 
